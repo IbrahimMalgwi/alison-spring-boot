60250ae92886e112c17e64ece4c3b0bd99c67e82
Ibrahim Gana Malgwi <ibrahimganamalgwi@outlook.com>
2023-03-26 01:56:55 +0100

    added html and css files

--- src/main/java/com/example/springbootapp/controller/GreetingController.java
+++ src/main/java/com/example/springbootapp/controller/GreetingController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-
     @Autowired
     private GreetingService greetingService;
 
--- src/main/java/com/example/springbootapp/controller/TestController.java
+++ src/main/java/com/example/springbootapp/controller/TestController.java
@@ -1,16 +1,17 @@
-//package com.example.springbootapp.controller;
-//
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("api/v1/")
-//public class TestController {
-//
-//    @RequestMapping(value = "hello/{name}", method = RequestMethod.GET)
-//    public String hello(@PathVariable(value = "") String name){
-//        return "Hello welcome " + name;
-//    }
-//}
+package com.example.springbootapp.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/")
+public class TestController {
+
+    @RequestMapping(value = "welcome/{name}", method = RequestMethod.GET)
+    public String welcome(@PathVariable(value = "") String name){
+        return "Hi welcome " + name;
+    }
+}
